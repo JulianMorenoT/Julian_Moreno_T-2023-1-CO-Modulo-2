d9adc08311d53cd20cac5bc65a9e7ad86eb889fe
Julian Moreno <Julian.Moreno@fundacion-jala.org>
2023-03-04 12:57:12 -0500

    Update dinosaur.py

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -4,21 +4,31 @@ from pygame.sprite import Sprite
 from dino_runner.utils.constants import RUNNING
 
 class Dinosaur(Sprite):
-
+    X_POS = 80
+    Y_POS = 310
+    
     def __init__(self):
         self.image = RUNNING[0]
         self.dino_rect = self.image.get_rect()
-        self.dino_rect.x = 80
-        self.dino_rect.y = 310
-        self.image1 = RUNNING[1]
-        self.dino_rect1 = self.image1.get_rect()
-        self.dino_rect1.x = 80
-        self.dino_rect1.y = 310
-
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+        self.step_index = 0
+        self.dino_run = True
+        self.dino_jump = False
 
     def update(self):
-        pass
+        if self.dino_run:
+            self.run()
 
+        if self.step_index >= 10:
+            self.step_index = 0
+
+    def run(self):
+        self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+        self.step_index += 1
+        
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
-        screen.blit(self.image1, (self.dino_rect1.x, self.dino_rect1.y))
