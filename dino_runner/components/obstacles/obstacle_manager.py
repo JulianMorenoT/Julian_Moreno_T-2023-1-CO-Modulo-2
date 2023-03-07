3a9c572aa8edf9ff3d53df9a6f7611bb04a65b7a
Julian Moreno <Julian.Moreno@fundacion-jala.org>
2023-03-07 14:51:05 -0500

    Update obstacle_manager.py

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,26 +1,35 @@
 import pygame
 
-from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.components.obstacles.cactus import Cactus, Cactus1
+from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS, BIRD
 
 
 class ObstacleManager:
  
     def __init__(self):
         self.obstacles = []
+        self.cactus_small = False
+
+        self.obstacles1 = []
+        self.pterodactyls = False
 
     def update(self, game):
 
-        if len(self.obstacles) == 0:
+        if len(self.obstacles) == 0 and not self.cactus_small:
             cactus = Cactus(SMALL_CACTUS)
             self.obstacles.append(cactus)
+            self.cactus_small = True
+        elif len(self.obstacles) == 0 and self.cactus_small:
+            cactus = Cactus1(LARGE_CACTUS)
+            self.obstacles.append(cactus)
+            self.cactus_small = False
 
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)   
 
             if game.player.dino_rect.colliderect(obstacle.rect):
                 game.playing = False
-                
+
     def draw(self, screen):
         for obstacle in self.obstacles:
             obstacle.draw(screen)
