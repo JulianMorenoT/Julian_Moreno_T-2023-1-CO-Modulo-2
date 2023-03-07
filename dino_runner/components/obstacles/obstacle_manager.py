2282610dac181da72c5dcb2c8e58a8117a5d6094
Julian Moreno <Julian.Moreno@fundacion-jala.org>
2023-03-06 22:40:02 -0500

    Update obstacle_manager.py

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -3,6 +3,7 @@ import pygame
 from dino_runner.components.obstacles.cactus import Cactus
 from dino_runner.utils.constants import SMALL_CACTUS
 
+
 class ObstacleManager:
  
     def _init_(self):
@@ -16,9 +17,9 @@ class ObstacleManager:
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)   
 
-            if game.player.dino_rect.colliderect(obstacle.rect):
-                game.playing = False
-                break
+            #if game.player.dino_rect.colliderect(obstacle.rect):
+            #    game.playing = False
+            #    break
                 
     def draw(self, screen):
         for obstacle in self.obstacles:
