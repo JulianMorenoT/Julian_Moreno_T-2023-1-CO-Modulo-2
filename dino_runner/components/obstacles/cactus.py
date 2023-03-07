6bc8ae58056f55ca4f2e70eaee325bca2000e4af
Julian Moreno <Julian.Moreno@fundacion-jala.org>
2023-03-07 14:51:03 -0500

    Update cactus.py

--- dino_runner/components/obstacles/cactus.py
+++ dino_runner/components/obstacles/cactus.py
@@ -8,4 +8,12 @@ class Cactus(Obstacle):
     def __init__(self, image):
         self.type = random.randint(0, 2)
         super().__init__(image, self.type)
-        self.rect.y = 325
+        self.rect.y = 330
+
+class Cactus1(Obstacle):
+
+    def __init__(self, image):
+        self.type = random.randint(0, 2)
+        super().__init__(image, self.type)
+        self.rect.y = 305
+   
